40c34c96e5b4d2c42fc70c135c5f9f10405b352e
RaykieZhang <grinningrich@163.com>
2021-08-13 22:27:59 +0800

    update: http trigger

--- src/code/index.js
+++ src/code/index.js
@@ -2,22 +2,13 @@ const { Server } = require('@webserverless/fc-express')
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
-const { execSync } = require('child_process')
 
 const app = express()
-const staticBasePath = './dist/static/'
 
-app.all('/', (req, resp) => {
-  resp.setHeader('Content-Type', 'text/html')
-  try {
-    console.log('\n***test fc internal endpoint***')
-    const res = execSync('curl --connect-timeout 3 https://1.cn-hangzhou-internal.fc.aliyuncs.com')
-    console.log()
-    console.log(res.toString('utf8'))
-  } catch (e) {
-    console.log('error when exec curl command.')
-  }
-  resp.send(fs.readFileSync('./dist/index.html', 'utf8'))
+const staticBasePath = './dist'
+
+app.get('/', (req, resp) => {
+  resp.sendFile(__dirname + '/dist/index.html')
 })
 
 app.all('/*.js', (req, resp) => {
@@ -35,9 +26,22 @@ app.all('/*.css', (req, resp) => {
 app.all('/*.png', (req, resp) => {
   const filePath = path.join(staticBasePath, req.path)
   resp.setHeader('Content-Type', 'image/png')
-  resp.send(fs.readFileSync(filePath, 'utf8'))
+  resp.send(fs.readFileSync(filePath))
 })
 
+app.all('/*.woff', (req, resp) => {
+  const filePath = path.join(staticBasePath, req.path)
+  resp.setHeader('Content-Type', 'application/x-font-woff')
+  resp.send(fs.readFileSync(filePath))
+})
+
+app.all('/*.ttf', (req, resp) => {
+  const filePath = path.join(staticBasePath, req.path)
+  resp.setHeader('Content-Type', 'font/ttf')
+  resp.send(fs.readFileSync(filePath))
+})
+
+// app.listen(3000, () => console.log('serving on http://localhost:3000/'))
 const server = new Server(app)
 
 module.exports.handler = function (req, res, context) {
