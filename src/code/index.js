94a70cb632c1a2c32fc742c23cb1b992caa73c5a
RaykieZhang <grinningrich@163.com>
2021-08-18 09:17:39 +0800

    static CORS version

--- src/code/index.js
+++ src/code/index.js
@@ -5,41 +5,43 @@ const path = require('path')
 
 const app = express()
 
-const staticBasePath = './dist'
+// const staticBasePath = './dist'
 
-app.get('/', (req, resp) => {
-  resp.sendFile(__dirname + '/dist/index.html')
-})
+app.use(express.static(path.join(__dirname, 'dist')))
 
-app.all('/*.js', (req, resp) => {
-  const filePath = path.join(staticBasePath, req.path)
-  resp.setHeader('Content-Type', 'text/javascript')
-  resp.send(fs.readFileSync(filePath, 'utf8'))
-})
+// app.get('/', (req, resp) => {
+//   resp.sendFile(__dirname + '/dist/index.html')
+// })
 
-app.all('/*.css', (req, resp) => {
-  const filePath = path.join(staticBasePath, req.path)
-  resp.setHeader('Content-Type', 'text/css')
-  resp.send(fs.readFileSync(filePath, 'utf8'))
-})
+// app.all('/*.js', (req, resp) => {
+//   const filePath = path.join(staticBasePath, req.path)
+//   resp.setHeader('Content-Type', 'text/javascript')
+//   resp.send(fs.readFileSync(filePath, 'utf8'))
+// })
 
-app.all('/*.png', (req, resp) => {
-  const filePath = path.join(staticBasePath, req.path)
-  resp.setHeader('Content-Type', 'image/png')
-  resp.send(fs.readFileSync(filePath))
-})
+// app.all('/*.css', (req, resp) => {
+//   const filePath = path.join(staticBasePath, req.path)
+//   resp.setHeader('Content-Type', 'text/css')
+//   resp.send(fs.readFileSync(filePath, 'utf8'))
+// })
 
-app.all('/*.woff', (req, resp) => {
-  const filePath = path.join(staticBasePath, req.path)
-  resp.setHeader('Content-Type', 'application/x-font-woff')
-  resp.send(fs.readFileSync(filePath))
-})
+// app.all('/*.png', (req, resp) => {
+//   const filePath = path.join(staticBasePath, req.path)
+//   resp.setHeader('Content-Type', 'image/png')
+//   resp.send(fs.readFileSync(filePath))
+// })
 
-app.all('/*.ttf', (req, resp) => {
-  const filePath = path.join(staticBasePath, req.path)
-  resp.setHeader('Content-Type', 'font/ttf')
-  resp.send(fs.readFileSync(filePath))
-})
+// app.all('/*.woff', (req, resp) => {
+//   const filePath = path.join(staticBasePath, req.path)
+//   resp.setHeader('Content-Type', 'application/x-font-woff')
+//   resp.send(fs.readFileSync(filePath))
+// })
+
+// app.all('/*.ttf', (req, resp) => {
+//   const filePath = path.join(staticBasePath, req.path)
+//   resp.setHeader('Content-Type', 'font/ttf')
+//   resp.send(fs.readFileSync(filePath))
+// })
 
 // app.listen(3000, () => console.log('serving on http://localhost:3000/'))
 const server = new Server(app)
